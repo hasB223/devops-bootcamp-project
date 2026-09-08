143d7c088343e2c5c07704d1da1caaf43ee43936
hasB223 <has.bc3@gmail.com>
2026-09-08 14:57:26 +0800

    feat(terraform): add GitHub Actions OIDC provider and role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -77,3 +77,88 @@ resource "aws_iam_instance_profile" "controller_profile" {
     Name = "devops-controller-profile"
   }
 }
+
+# ==============================================================================
+# GitHub Actions OIDC Provider & Role for Automated ECR Image Publishing
+# ==============================================================================
+resource "aws_iam_openid_connect_provider" "github" {
+  url            = "https://token.actions.githubusercontent.com"
+  client_id_list = ["sts.amazonaws.com"]
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1",
+    "1c5824a6f5a8794cb5c879d799042b5a5101a613"
+  ]
+
+  tags = {
+    Name = "github-actions-oidc-provider"
+  }
+}
+
+data "aws_iam_policy_document" "github_actions_assume_role" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:hasB223/devops-bootcamp-project:ref:refs/heads/main"]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions" {
+  name               = "devops-github-actions-role"
+  assume_role_policy = data.aws_iam_policy_document.github_actions_assume_role.json
+
+  tags = {
+    Name = "devops-github-actions-role"
+  }
+}
+
+data "aws_iam_policy_document" "github_actions_ecr_push" {
+  statement {
+    sid       = "ECRGetAuthorizationToken"
+    effect    = "Allow"
+    actions   = ["ecr:GetAuthorizationToken"]
+    resources = ["*"]
+  }
+
+  statement {
+    sid    = "ECRPushImageClaims"
+    effect = "Allow"
+    actions = [
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ecr:PutImage",
+      "ecr:InitiateLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:CompleteLayerUpload"
+    ]
+    resources = [aws_ecr_repository.app.arn]
+  }
+}
+
+resource "aws_iam_policy" "github_actions_ecr" {
+  name        = "devops-github-actions-ecr-policy"
+  description = "Scoped policy allowing GitHub Actions OIDC workflow to push container images"
+  policy      = data.aws_iam_policy_document.github_actions_ecr_push.json
+}
+
+resource "aws_iam_role_policy_attachment" "github_actions_ecr" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = aws_iam_policy.github_actions_ecr.arn
+}
+
