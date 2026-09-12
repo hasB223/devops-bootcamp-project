cb95f3a918a3c47c2f443063c7bf4ac9bc5b77e0
hasB223 <has.bc3@gmail.com>
2026-09-12 12:35:40 +0800

    fix(iam): grant ec2:Describe*, iam:Get*/List*, ecr:Describe*/Get*/List*, s3:Get*/List* to lifecycle role

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -342,27 +342,12 @@ resource "aws_iam_role_policy_attachment" "github_actions_ssm" {
 # GitHub Actions Policy for Platform Lifecycle Automation (Park & Unpark)
 # ==============================================================================
 data "aws_iam_policy_document" "github_actions_lifecycle" {
-  # 1. AWS ec2:Describe* APIs (AWS specification: Describe* does not support resource-level permissions or condition keys)
+  # 1. AWS ec2:Describe* APIs (All EC2 Describe* APIs are read-only and require Resource = ["*"])
   statement {
     sid    = "EC2DescribeReadPlatformState"
     effect = "Allow"
     actions = [
-      "ec2:DescribeInstances",
-      "ec2:DescribeInstanceStatus",
-      "ec2:DescribeInstanceCreditSpecifications",
-      "ec2:DescribeNatGateways",
-      "ec2:DescribeAddresses",
-      "ec2:DescribeRouteTables",
-      "ec2:DescribeVpcEndpoints",
-      "ec2:DescribeVpcs",
-      "ec2:DescribeSubnets",
-      "ec2:DescribeSecurityGroups",
-      "ec2:DescribeSecurityGroupRules",
-      "ec2:DescribeAvailabilityZones",
-      "ec2:DescribeImages",
-      "ec2:DescribeInternetGateways",
-      "ec2:DescribeNetworkInterfaces",
-      "ec2:DescribeTags"
+      "ec2:Describe*"
     ]
     resources = ["*"]
   }
@@ -489,19 +474,8 @@ data "aws_iam_policy_document" "github_actions_lifecycle" {
     sid    = "IAMStateReadRefresh"
     effect = "Allow"
     actions = [
-      "iam:GetRole",
-      "iam:GetRolePolicy",
-      "iam:ListRolePolicies",
-      "iam:ListAttachedRolePolicies",
-      "iam:ListRoleTags",
-      "iam:GetPolicy",
-      "iam:GetPolicyVersion",
-      "iam:ListPolicyVersions",
-      "iam:ListPolicyTags",
-      "iam:GetInstanceProfile",
-      "iam:ListInstanceProfileTags",
-      "iam:GetOpenIDConnectProvider",
-      "iam:ListOpenIDConnectProviderTags"
+      "iam:Get*",
+      "iam:List*"
     ]
     resources = [
       "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/devops-*",
@@ -516,10 +490,9 @@ data "aws_iam_policy_document" "github_actions_lifecycle" {
     sid    = "ECRStateReadRefresh"
     effect = "Allow"
     actions = [
-      "ecr:DescribeRepositories",
-      "ecr:GetRepositoryPolicy",
-      "ecr:GetLifecyclePolicy",
-      "ecr:ListTagsForResource"
+      "ecr:Describe*",
+      "ecr:Get*",
+      "ecr:List*"
     ]
     resources = [
       aws_ecr_repository.app.arn
@@ -531,20 +504,8 @@ data "aws_iam_policy_document" "github_actions_lifecycle" {
     sid    = "S3AnsibleBucketReadRefresh"
     effect = "Allow"
     actions = [
-      "s3:GetBucketAcl",
-      "s3:GetBucketCORS",
-      "s3:GetBucketWebsite",
-      "s3:GetBucketVersioning",
-      "s3:GetAccelerationConfiguration",
-      "s3:GetBucketRequestPayment",
-      "s3:GetBucketLogging",
-      "s3:GetLifecycleConfiguration",
-      "s3:GetReplicationConfiguration",
-      "s3:GetEncryptionConfiguration",
-      "s3:GetBucketObjectLockConfiguration",
-      "s3:GetBucketTagging",
-      "s3:GetBucketPolicy",
-      "s3:GetBucketPublicAccessBlock"
+      "s3:Get*",
+      "s3:List*"
     ]
     resources = [
       aws_s3_bucket.ansible_ssm.arn,
